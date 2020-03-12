08003ea825731ef67514b524e126f316fdcac75d
unknown <robert.kami88@gmail.com>
2020-03-12 23:43:03 +0100

    Created new feature Sync. Pre build bug fix

---
+++ src/core/console/observer/comperators/trackLoaded.js
@@ -0,0 +1,23 @@
+import STATUS from  "./../STATUS";
+
+export default function checkPitchChange(prev, current){
+    prev = prev.console;
+    current = current.console;
+    let response = null;
+    for(let channelName of Object.keys(prev.channel)){
+        const prevValueSource = prev.channel[channelName].track.source;
+        const prevValueId = prev.channel[channelName].track.id;
+        const currentValueSource = current.channel[channelName].track.source;
+        const currentValueID = current.channel[channelName].track.id;
+        if( prevValueSource !== currentValueSource || prevValueId !== currentValueID)
+            {
+                return {
+                    status : STATUS.TRACK_LOADED,
+                    channel : channelName,
+                    prevValue : prev.channel[channelName].track,
+                    currentValue : current.channel[channelName].track
+                }                
+            }
+    }
+    return response;
+}
