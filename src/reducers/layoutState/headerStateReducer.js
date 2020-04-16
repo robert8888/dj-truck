4fee4b7e76bfb6b2180057ca426d3aaa97431b06
unknown <robert.kami88@gmail.com>
2020-04-16 19:19:40 +0200

    Menu bar feature created

---
+++ src/reducers/layoutState/headerStateReducer.js
@@ -0,0 +1,14 @@
+import {ACTIONS} from "./../../actions";
+
+const initState = {
+    disable: false,
+    hidden: false,
+    sticki: true,
+}
+
+export default function headerState(state = initState, action){
+    switch(action.type){
+
+        default: return state;
+    }
+}
