8eedede2fcb0ddfc5b0b614cbb08c10a85c3aa4f
unknown <robert.kami88@gmail.com>
2020-05-29 03:23:22 +0200

    Adding compresion to static server

--- server.js
+++ server.js
@@ -1,13 +1,45 @@
 require("dotenv").config();
 const express = require('express');
+var expressStaticGzip = require('express-static-gzip');
 const path = require('path');
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+
+const pattern = /.*\.(png|js|css)/
+// app.use((req, res, next)=>{
+//     if(!req.originalUrl.match(pattern)){
+//         next();
+//         return;
+//     }
+
+//     req.originalUrl += ".br"
+//     req._parsedUrl.pathname += ".br"
+//     req._parsedUrl.path += ".br"
+//     req._parsedUrl.href += ".br"
+//     req._parsedUrl._raw += ".br"
+//     req.url += ".br"
+//     next();
+// })
+
+// app.use((req, res , next) => {
+//     console.log([req.originalUrl,
+//         req._parsedUrl.pathname,
+//         req._parsedUrl.path,
+//         req._parsedUrl.href,
+//         req._parsedUrl._raw,
+//         req.url,])
+//     next();
+// })
+
 app.use(express.static(path.join(__dirname, 'build')));
 
+app.use(expressStaticGzip(path.join(__dirname, 'build'), {
+    enableBrotli :true,
+}))
+
 app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'build', 'index.html.gz'));
 });
 
 app.listen(PORT, ()=>{
