9568ff96deb1fd632879ddaf6af22a8598e00bc0
unknown <robert.kami88@gmail.com>
2020-05-30 00:32:04 +0200

    Fix sc url slash bug

--- src/apis/sc/scApi.js
+++ src/apis/sc/scApi.js
@@ -1,10 +1,10 @@
 const apiUrl = process.env.REACT_APP_SC_API_URL;
 const maxResults = 10
 
-export async function search(query = "", limit = maxResults){
+export async function search(query = "", limit = maxResults) {
 
-    const comand =  "/search?q=";
-    const options = '&maxResults=' + limit ;
+    const comand = "/search?q=";
+    const options = '&maxResults=' + limit;
     const url = apiUrl + comand + encodeURI(query) + options;
     let response = await fetch(url);
     return await response.json();
@@ -12,12 +12,13 @@ export async function search(query = "", limit = maxResults){
 
 export default {
     search,
-    
-    getUrl : (id) => {
-        return apiUrl + "/download?id=" + id ; 
+
+    getUrl: (id) => {
+        const slash = (!apiUrl.endsWith("/")) ? "/" : "";
+        return apiUrl + `${slash}download?id=` + id;
     },
 
-    getUrlToExternall : (id) => {
+    getUrlToExternall: (id) => {
         return "https://www.youtube.com/watch?v=" + id;
     }
 } 
