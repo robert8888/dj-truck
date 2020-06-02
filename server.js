5b93799447c31c3646d816c8503ac5b93fdff194
unknown <robert.kami88@gmail.com>
2020-06-02 18:20:20 +0200

    reapit handling by server sub routes

--- server.js
+++ server.js
@@ -11,11 +11,14 @@ app.use(expressStaticGzip(path.join(__dirname, 'build'), {
     orderPreference: ['gzip'],
     maxAge: 0,
     serveStatic: {
-        maxAge: 0,            
+        maxAge: 0,
     }
 }))
 
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/build/index.html'));
+});
 
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
     console.log("Server started on port: " + PORT)
 });
